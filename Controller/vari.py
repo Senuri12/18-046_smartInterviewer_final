6f073562adef313f0c4ce107da2e1a531c30832d
Senuri12 <senuseneviratneit@gmail.com>
2018-11-06 23:11:16 +0530

    hh

--- Controller/vari.py
+++ Controller/vari.py
@@ -1,14 +1,5 @@
 global userId
 userId = 'uid008'
 global sessionId
-<<<<<<< HEAD
-sessionId = '1'
-=======
-
 sessionId = '1'
 
-
-
-sessionId = '9'
-
->>>>>>> 3c02ac1e99fe7b41f03eda91f157f42e1a211e03
