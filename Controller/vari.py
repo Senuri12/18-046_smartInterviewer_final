b1a458560984d37d05ec397cec62646edb6d9c47
Senuri12 <senuseneviratneit@gmail.com>
2018-11-11 13:12:36 +0530

    commit

--- Controller/vari.py
+++ Controller/vari.py
@@ -1,12 +1,4 @@
 global userId
-userId = 'uid001'
+userId = 'uid004'
 global sessionId
-<<<<<<< HEAD
-sessionId = '1'
-=======
-<<<<<<< HEAD
-sessionId = '1'
-=======
-sessionId = '10'
->>>>>>> 4e892fef05aaabdd6f9e3a7370a5526eda97dacd
->>>>>>> f13d50d62aef9a98a02086e1d9d387bad26e1660
+sessionId = '2'
